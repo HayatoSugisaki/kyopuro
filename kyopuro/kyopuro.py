bf76ab6cf0dfb108c9860d9a88265052513921a8
HayatoSugisaki <74641895+HayatoSugisaki@users.noreply.github.com>
2023-11-13 02:57:11 +0900

    Update kyopuro.py

--- kyopuro/kyopuro.py
+++ kyopuro/kyopuro.py
@@ -3,9 +3,6 @@ from collections import deque
 def help():
     print("This is a personal library for programming contest by Hayato Sugisaki.")
 
-def hello():
-    print('hello')
-
 #幅優先探索で各頂点への最短距離を出す
 def shortpath(N_vertex, edge, start):
     
@@ -25,9 +22,6 @@ def shortpath(N_vertex, edge, start):
                 distance[v] = distance[current] + 1
 
     return(distance)
-
-def hi():
-    print(hi)
 
 #幅優先探索で二部グラフか確かめる
 def checkbpt(N_vertex, edge):
